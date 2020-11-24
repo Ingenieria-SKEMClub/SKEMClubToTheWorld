340eb2e504910c1757d0d2a74cf130063bec5dd3
Gabelox14 <61789851+Gabelox14@users.noreply.github.com>
2020-11-23 20:10:16 -0600

    Webpack include
    Webpack include

---
+++ src/materializefunctions.js
@@ -0,0 +1,12 @@
+M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+
+
